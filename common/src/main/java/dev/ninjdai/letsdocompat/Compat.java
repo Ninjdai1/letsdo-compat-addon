8f0e56141effb14cf9b08a0ccd1fb135d4b0d657
Ninjdai <ninjdai@duck.com>
2024-12-27 22:39:41 +0100

    Gradle files update

--- common/src/main/java/dev/ninjdai/letsdocompat/Compat.java
+++ common/src/main/java/dev/ninjdai/letsdocompat/Compat.java
@@ -4,10 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class Compat {
-    public static final String MOD_ID = "letsdoaddoncompat";
+    public static final String MOD_ID = "letsdocompat";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static void init() {
-        // Write common init code here.
+        LOGGER.info("You shall not Json my recipes !");
     }
 }
