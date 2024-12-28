4a2731069623ae3ad50903a28de87c9ec2f26fc9
Ninjdai <ninjdai@duck.com>
2024-12-28 14:19:41 +0100

    fix: gradle not building and wrong containers when reading FD recipes

--- common/src/main/java/dev/ninjdai/letsdocompat/mixin/RecipeManagerMixin.java
+++ common/src/main/java/dev/ninjdai/letsdocompat/mixin/RecipeManagerMixin.java
@@ -3,9 +3,13 @@ package dev.ninjdai.letsdocompat.mixin;
 import com.google.gson.*;
 import dev.ninjdai.letsdocompat.Compat;
 import dev.ninjdai.letsdocompat.DoAddonExpectPlatform;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.crafting.RecipeManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -47,15 +51,17 @@ public abstract class RecipeManagerMixin {
                     letsDoCookingPotRecipe.add("ingredients", jsonRecipe.getAsJsonArray("ingredients"));
                     if (jsonRecipe.has("container")) letsDoCookingPotRecipe.add("container", jsonRecipe.get("container"));
                     else {
-                        String item;
-                        switch (jsonRecipe.get("recipe_book_tab").getAsString()){
-                            case "misc": continue;
-                            case "drinks": item = "minecraft:glass_bottle";break;
-                            default: item = "minecraft:bowl";break;
+                        try {
+                            Item i = BuiltInRegistries.ITEM.get(new ResourceLocation(jsonRecipe.get("result").getAsJsonObject().get("item").getAsString()));
+                            if (i.hasCraftingRemainingItem()) {
+                                JsonObject container = new JsonObject();
+                                container.add("item", new JsonPrimitive(i.getCraftingRemainingItem().arch$registryName().toString()));
+                                letsDoCookingPotRecipe.add("container", container);
+                            } else continue;
+                        } catch (Exception e) {
+                            Compat.LOGGER.error("Error getting item from Item Registry: {}", e.getMessage());
+                            continue;
                         }
-                        JsonObject container = new JsonObject();
-                        container.add("item", new JsonPrimitive(item));
-                        letsDoCookingPotRecipe.add("container", container);
                     }
                     letsDoCookingPotRecipe.add("result", jsonRecipe.get("result"));
                     ResourceLocation id = new ResourceLocation(Compat.MOD_ID, "farmersdelight/" + resourceLocation.getPath());
