045033eab7884d63f0ee9879e5317591114c0a78
Ninjdai <ninjdai@duck.com>
2025-03-20 15:31:21 +0100

    refactor: move json manipulation out of mixin function

---
+++ common/src/main/java/dev/ninjdai/letsdocompat/RecipeJsonUtil.java
@@ -0,0 +1,57 @@
+package dev.ninjdai.letsdocompat;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+
+public final class RecipeJsonUtil {
+    public static JsonObject generateFDCookingPotFromLDCookingPot(JsonObject jsonRecipe) {
+        JsonObject FDCookingPotRecipe = new JsonObject();
+        FDCookingPotRecipe.add("type", new JsonPrimitive("farmersdelight:cooking"));
+        FDCookingPotRecipe.add("recipe_book_tab", new JsonPrimitive("meals"));
+        FDCookingPotRecipe.add("ingredients", jsonRecipe.getAsJsonArray("ingredients"));
+        try {
+            JsonObject ldContainer = jsonRecipe.getAsJsonObject("container");
+            if (!ldContainer.get("required").getAsBoolean()) {
+                FDCookingPotRecipe.add("container", ldContainer.get("item"));
+            }
+        } catch(Exception e) {
+            Compat.LOGGER.error("Error while reading recipe container");
+        }
+        FDCookingPotRecipe.add("result", jsonRecipe.get("result"));
+        return FDCookingPotRecipe;
+    }
+
+    public static JsonObject generateLDCookingPotFromFDCookingPot(JsonObject jsonRecipe) {
+        JsonObject letsDoCookingPotRecipe = new JsonObject();
+        letsDoCookingPotRecipe.add("type", new JsonPrimitive("farm_and_charm:pot_cooking"));
+        letsDoCookingPotRecipe.add("ingredients", jsonRecipe.getAsJsonArray("ingredients"));
+        if (jsonRecipe.has("container")) {
+            JsonObject containerObject = new JsonObject();
+            containerObject.add("required", new JsonPrimitive(true));
+            containerObject.add("item", jsonRecipe.get("container"));
+            letsDoCookingPotRecipe.add("container", containerObject);
+        }
+        else {
+            try {
+                Item i = BuiltInRegistries.ITEM.get(new ResourceLocation(jsonRecipe.get("result").getAsJsonObject().get("item").getAsString()));
+                if (i.getCraftingRemainingItem() != null) {
+                    JsonObject container = new JsonObject();
+                    container.add("required", new JsonPrimitive(true));
+                    JsonObject itemObject = new JsonObject();
+                    itemObject.add("item", new JsonPrimitive(i.getCraftingRemainingItem().arch$registryName().toString()));
+                    container.add("item", itemObject);
+                    letsDoCookingPotRecipe.add("container", container);
+                } else return null;
+            } catch (Exception e) {
+                Compat.LOGGER.error("Error getting item from Item Registry: {}", e.getMessage());
+                return null;
+            }
+        }
+        letsDoCookingPotRecipe.add("result", jsonRecipe.get("result"));
+        letsDoCookingPotRecipe.add("requiresLearning", new JsonPrimitive(false));
+        return letsDoCookingPotRecipe;
+    }
+}
--- common/src/main/java/dev/ninjdai/letsdocompat/mixin/RecipeManagerMixin.java
+++ common/src/main/java/dev/ninjdai/letsdocompat/mixin/RecipeManagerMixin.java
@@ -3,6 +3,7 @@ package dev.ninjdai.letsdocompat.mixin;
 import com.google.gson.*;
 import dev.ninjdai.letsdocompat.Compat;
 import dev.ninjdai.letsdocompat.DoAddonExpectPlatform;
+import dev.ninjdai.letsdocompat.RecipeJsonUtil;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
@@ -10,6 +11,7 @@ import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.crafting.RecipeManager;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -29,53 +31,13 @@ public abstract class RecipeManagerMixin {
             String recipeType = jsonRecipe.get("type").getAsString();
             if (recipeType.equals("farm_and_charm:pot_cooking")) {
                 if (DoAddonExpectPlatform.isModLoaded("farmersdelight")) {
-                    JsonObject FDCookingPotRecipe = new JsonObject();
-                    FDCookingPotRecipe.add("type", new JsonPrimitive("farmersdelight:cooking"));
-                    FDCookingPotRecipe.add("recipe_book_tab", new JsonPrimitive("meals"));
-                    FDCookingPotRecipe.add("ingredients", jsonRecipe.getAsJsonArray("ingredients"));
-                    try {
-                        JsonObject ldContainer = jsonRecipe.getAsJsonObject("container");
-                        if (!ldContainer.get("required").getAsBoolean()) {
-                            FDCookingPotRecipe.add("container", ldContainer.get("item"));
-                        }
-                    } catch(Exception e) {
-                        Compat.LOGGER.error("Error while reading recipe container");
-                    }
-                    FDCookingPotRecipe.add("result", jsonRecipe.get("result"));
                     ResourceLocation id = new ResourceLocation(Compat.MOD_ID, "farm_and_charm/" + resourceLocation.getPath());
-                    tMap.put(id, FDCookingPotRecipe);
+                    tMap.put(id, RecipeJsonUtil.generateFDCookingPotFromLDCookingPot(jsonRecipe));
                 }
             } else if (recipeType.equals("farmersdelight:cooking")) {
                 if (DoAddonExpectPlatform.isModLoaded("farm_and_charm")) {
-                    JsonObject letsDoCookingPotRecipe = new JsonObject();
-                    letsDoCookingPotRecipe.add("type", new JsonPrimitive("farm_and_charm:pot_cooking"));
-                    letsDoCookingPotRecipe.add("ingredients", jsonRecipe.getAsJsonArray("ingredients"));
-                    if (jsonRecipe.has("container")) {
-                        JsonObject containerObject = new JsonObject();
-                        containerObject.add("required", new JsonPrimitive(true));
-                        containerObject.add("item", jsonRecipe.get("container"));
-                        letsDoCookingPotRecipe.add("container", containerObject);
-                    }
-                    else {
-                        try {
-                            Item i = BuiltInRegistries.ITEM.get(new ResourceLocation(jsonRecipe.get("result").getAsJsonObject().get("item").getAsString()));
-                            if (i.hasCraftingRemainingItem()) {
-                                JsonObject container = new JsonObject();
-                                container.add("required", new JsonPrimitive(true));
-                                JsonObject itemObject = new JsonObject();
-                                itemObject.add("item", new JsonPrimitive(i.getCraftingRemainingItem().arch$registryName().toString()));
-                                container.add("item", itemObject);
-                                letsDoCookingPotRecipe.add("container", container);
-                            } else continue;
-                        } catch (Exception e) {
-                            Compat.LOGGER.error("Error getting item from Item Registry: {}", e.getMessage());
-                            continue;
-                        }
-                    }
-                    letsDoCookingPotRecipe.add("result", jsonRecipe.get("result"));
-                    letsDoCookingPotRecipe.add("requiresLearning", new JsonPrimitive(false));
                     ResourceLocation id = new ResourceLocation(Compat.MOD_ID, "farmersdelight/" + resourceLocation.getPath());
-                    tMap.put(id, letsDoCookingPotRecipe);
+                    tMap.put(id, RecipeJsonUtil.generateLDCookingPotFromFDCookingPot(jsonRecipe));
                 }
             }
         }
